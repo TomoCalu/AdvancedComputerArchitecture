747320e418cb9a6ab4c646f9c058e4d255fb104b
Tomislav Ćalušić <TomoCalu@gmail.com>
2018-06-11 20:19:52 +0200

    Added more images, and made code a bit cleaner

--- Assignment 2/CannyEdgeParallel.cpp
+++ Assignment 2/CannyEdgeParallel.cpp
@@ -1,89 +1,82 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include <stdlib.h>
-#include <stdio.h>
 #include <thread>
 #include <chrono>
 #include <iostream>
 
 
-#define IMAGE_LOCATION "Examples/lena.bmp"
+#define IMAGE_LOCATION "Examples/forest.jpg"
 #define FIRST_THRESHOLD 50
 #define SECOND_THRESHOLD 150
 #define KERNEL_SIZE 3
 #define NUMBER_OF_THREADS 4
 
-using namespace cv;
-using namespace std;
-
-Mat inputImage, outputImage;
-vector<Mat> inputImageBlocks, outputImageBlock (NUMBER_OF_THREADS);
+cv::Mat inputImage, outputImage;
+std::vector<cv::Mat> inputImageBlocks, outputImageBlock (NUMBER_OF_THREADS);
 int blockNumber = 0;
 
 class Timer
 {
-public:
-	Timer() : beg_(clock_::now()) {}
-	void reset() { beg_ = clock_::now(); }
-	double elapsed() const {
-		return std::chrono::duration_cast<std::chrono::microseconds>
-			(clock_::now() - beg_).count();
-	}
+	public:
+		Timer() : beg_(clock_::now()) {}
+		void reset() { beg_ = clock_::now(); }
+		double elapsed() const {
+			return std::chrono::duration_cast<std::chrono::microseconds>
+				(clock_::now() - beg_).count();
+		}
 
-private:
-	typedef std::chrono::high_resolution_clock clock_;
-	typedef std::chrono::duration<double, std::ratio<1> > second_;
-	std::chrono::time_point<clock_> beg_;
+	private:
+		typedef std::chrono::high_resolution_clock clock_;
+		typedef std::chrono::duration<double, std::ratio<1> > second_;
+		std::chrono::time_point<clock_> beg_;
 };
 
-void canny_edge_filter_for_block(Mat block, int threadID)
+void canny_edge_filter_for_block(cv::Mat block, int threadID)
 {
-	Mat blockGrayscale, edgeImage, blockOutput;
+	cv::Mat blockGrayscale, edgeImage, blockOutput;
 	blockOutput.create(block.size(), block.type());
-	cvtColor(block, blockGrayscale, CV_BGR2GRAY);
-	blur(blockGrayscale, edgeImage, Size(3, 3));
+	cv::cvtColor(block, blockGrayscale, CV_BGR2GRAY);
+	blur(blockGrayscale, edgeImage, cv::Size(3, 3));
 	Canny(edgeImage, edgeImage, FIRST_THRESHOLD, SECOND_THRESHOLD, KERNEL_SIZE);
 
-	blockOutput = Scalar::all(0);
+	blockOutput = cv::Scalar::all(0);
 
 	block.copyTo(blockOutput, edgeImage);
 	cvtColor(blockOutput, blockOutput, CV_BGR2GRAY);
 	outputImageBlock[threadID].push_back(blockOutput);
-	//imwrite("Examples/cany_edge_detection.jpg", blockOutput);
 }
 
 int main()
 {
 	double t1, t2;
 	Timer tmr;
-	thread threads[NUMBER_OF_THREADS];
+	std::thread threads[NUMBER_OF_THREADS];
 
-	inputImage = imread(IMAGE_LOCATION);
-
+	inputImage = cv::imread(IMAGE_LOCATION);
 	if (inputImage.empty()) {
-		printf("Image not found, please restart the program with different image path! \n");
+		std::cout << "Image not found, please restart the program with different image path!" << std::endl;
+		getchar();
+
 		return EXIT_FAILURE;
 	}
-	printf("Image found... Starting program\n");
+	std::cout << "Image found... Starting program\n" << std::endl;
 
-	Mat maskImage = inputImage.clone();
-
+	cv::Mat maskImage = inputImage.clone();
 	if (inputImage.rows % NUMBER_OF_THREADS == 0)
 	{
 		for (int x = 0; x < inputImage.rows; x += inputImage.rows / NUMBER_OF_THREADS)
 		{
-			inputImageBlocks.push_back(inputImage(Rect(0, x, (inputImage.cols), (inputImage.rows / NUMBER_OF_THREADS))).clone());
-			rectangle(maskImage, Point(0, x), Point(0 + (maskImage.cols) - 1, x + (maskImage.rows / NUMBER_OF_THREADS) - 1), CV_RGB(255, 0, 0), 1); // visualization
+			inputImageBlocks.push_back(inputImage(cv::Rect(0, x, (inputImage.cols), (inputImage.rows / NUMBER_OF_THREADS))).clone());
+			rectangle(maskImage, cv::Point(0, x), cv::Point(0 + (maskImage.cols) - 1, x + (maskImage.rows / NUMBER_OF_THREADS) - 1), CV_RGB(255, 0, 0), 1); // visualization
 			imshow("Mask image", maskImage); // visualization
 		}
 	}
-
 	outputImage.create(inputImage.size(), inputImage.type());
 
 	t1 = tmr.elapsed();
 	for (blockNumber = 0; blockNumber < inputImageBlocks.size(); blockNumber++)
 	{
-		threads[blockNumber]= thread(canny_edge_filter_for_block, inputImageBlocks.at(blockNumber), blockNumber);
+		threads[blockNumber]= std::thread(canny_edge_filter_for_block, inputImageBlocks.at(blockNumber), blockNumber);
 	}
 
 	for (blockNumber = 0; blockNumber < inputImageBlocks.size(); blockNumber++)
@@ -97,11 +90,11 @@ int main()
 		vconcat(outputImage, outputImageBlock.at(blockNumber + 2), outputImage);
 	}
 	t2 = tmr.elapsed();
-	imshow("Output image", outputImage);
 
-	cout << "Pthread sum time: " << t2 - t1 << endl << endl;
-	printf("Program has ended. Press any key to continue... \n");
-	waitKey(0);
+	imshow("Output image", outputImage);
+	std::cout << "Parralel canny edge time: " << t2 - t1 << std::endl << std::endl;
+	std::cout << "Program has ended. Press any key to continue... " << std::endl;
+	cv::waitKey(0);
 
 	return EXIT_SUCCESS;
 }
--- Assignment 2/CannyEdgeSequential.cpp
+++ Assignment 2/CannyEdgeSequential.cpp
@@ -1,48 +1,42 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
 #include <chrono>
 
-#define IMAGE_LOCATION "Examples/lena.bmp"
+#define IMAGE_LOCATION "Examples/forest.jpg"
 #define FIRST_THRESHOLD 50
 #define SECOND_THRESHOLD 150
 #define KERNEL_SIZE 3
 
-using namespace cv;
-using namespace std;
-
-Mat inputImage, outputImage, inputImageGrayscale, edgeImage;
+cv::Mat inputImage, outputImage, inputImageGrayscale, edgeImage;
 
 class Timer
 {
-public:
-	Timer() : beg_(clock_::now()) {}
-	void reset() { beg_ = clock_::now(); }
-	double elapsed() const {
-		return std::chrono::duration_cast<std::chrono::microseconds>
-			(clock_::now() - beg_).count();
-	}
+	public:
+		Timer() : beg_(clock_::now()) {}
+		void reset() { beg_ = clock_::now(); }
+		double elapsed() const {
+			return std::chrono::duration_cast<std::chrono::microseconds>
+				(clock_::now() - beg_).count();
+		}
 
-private:
-	typedef std::chrono::high_resolution_clock clock_;
-	typedef std::chrono::duration<double, std::ratio<1> > second_;
-	std::chrono::time_point<clock_> beg_;
+	private:
+		typedef std::chrono::high_resolution_clock clock_;
+		typedef std::chrono::duration<double, std::ratio<1> > second_;
+		std::chrono::time_point<clock_> beg_;
 };
 
-void canny_edge_filter(int, void*)
+void canny_edge_filter()
 {
-	blur(inputImageGrayscale, edgeImage, Size(3, 3));
+	blur(inputImageGrayscale, edgeImage, cv::Size(3, 3));
 
 	Canny(edgeImage, edgeImage, FIRST_THRESHOLD, SECOND_THRESHOLD, KERNEL_SIZE);
 
-	outputImage = Scalar::all(0);
+	outputImage = cv::Scalar::all(0);
 
 	inputImage.copyTo(outputImage, edgeImage);
 
 	cvtColor(outputImage, outputImage, CV_BGR2GRAY);
-	//imwrite("Examples/cany_edge_detection.jpg", outputImage);
 }
 
 int main() 
@@ -50,26 +44,28 @@ int main()
 	double t1, t2;
 	Timer tmr;
 
-	inputImage = imread(IMAGE_LOCATION);
+	inputImage = cv::imread(IMAGE_LOCATION);
 	if (inputImage.empty()) {
-		printf("Image not found, please restart the program with different image path! \n");
+		std::cout << "Image not found, please restart the program with different image path!" << std::endl;
+		getchar();
+
 		return EXIT_FAILURE;
 	}
-	printf("Image found... Starting program\n");
+	std::cout << "Image found... Starting program\n" << std::endl;
 
 	imshow("Input image", inputImage);
 
 	outputImage.create(inputImage.size(), inputImage.type());
 	cvtColor(inputImage, inputImageGrayscale, CV_BGR2GRAY);
 	t1 = tmr.elapsed();
-	canny_edge_filter(0, 0);
+	canny_edge_filter();
 	t2 = tmr.elapsed();
 	imshow("Output image", outputImage);
 
-	cout << "Pthread sum time: " << t2 - t1 << endl << endl;
+	std::cout << "Sequential canny edge time: " << t2 - t1 << std::endl << std::endl;
 
 	printf("Program has ended. Press any key to continue... \n");
-	waitKey(0);
+	cv::waitKey(0);
 
 	return EXIT_SUCCESS;
 }
